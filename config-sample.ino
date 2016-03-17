5c5378b7019f99b4866abe5537560791811c7dfa
Francis GILLES <francis@quai13.com>
2016-03-17 18:16:32 +0100

    modif config sample

--- config-sample.ino
+++ config-sample.ino
@@ -1,6 +1,8 @@
 #ifndef _config_
 #define _config_
 
-static const String allowedNumber = "";
+static const bool gsmdebug = false;
+
+static const String allowedNumber = "+33645970094";
 
 #endif // _config_
